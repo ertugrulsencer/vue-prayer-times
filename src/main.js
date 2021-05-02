83e48b2f00f714126810e990ebdba61242a4c7a1
Ertuğrul Sencer <teknoertugrul@gmail.com>
2021-05-02 19:50:35 +0300

    Axios entegrated

--- src/main.js
+++ src/main.js
@@ -3,7 +3,11 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import store from "./store";
 
+/* Axios */
+import axios from "axios";
+
 Vue.config.productionTip = false;
+Vue.prototype.$http = axios;
 
 new Vue({
   store,
